a35ff1f637bd0a04cb942260251d06db19477f55
hicfool <58881699+hicfool@users.noreply.github.com>
2025-01-14 11:59:59 +0000

    Removed/replaced useless stuff.
    Removed the delay variable and made it so the speed stops changing every time a char goes out of bounds. Now there's speed and SPD: speed is decreased by GetFrameTime() * 60 and resets to SPD when 0 (SPD is set at the start).
    Also did the same thing for the colors, which changed every time c[i].y - 8 > RHEIGHT. They're now set at the start and never change.
    That check only happens when speed is 0 or less now.

--- main.cpp
+++ main.cpp
@@ -13,8 +13,7 @@ int genRand(int min, int max, std::mt19937& gen) {
 }
 
 int main(int argc, char* argv[]) {
-    const int RWIDTH = (argc > 1) ? atoi(argv[1]) : 128;
-    const int RHEIGHT = (argc > 2) ? atoi(argv[2]) : 128;
+    const int RWIDTH = (argc > 1) ? atoi(argv[1]) : 128, RHEIGHT = (argc > 2) ? atoi(argv[2]) : 128;
     int windowWidth = 600, windowHeight = windowWidth;
     bool paused = false;
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
@@ -26,7 +25,7 @@ int main(int argc, char* argv[]) {
     std::random_device rd;
     std::mt19937 gen(rd());
 
-    struct Grid { std::vector<int>x, y; }; struct Grid grid;
+    struct Grid { std::vector<int> x, y; }; struct Grid grid;
     for (int i = 0; i < RWIDTH / 8; i++) {
         grid.x.push_back(i * 8);
         for (int j = 0; j < RHEIGHT / 8; j++) {
@@ -37,16 +36,15 @@ int main(int argc, char* argv[]) {
     const int MAX = (argc > 3) ? atoi(argv[3]) : HI(RWIDTH / 4, RHEIGHT / 4);
 
     struct C {
-        float x, y, speed, delay;
+        float x, y, speed;
         char type[2];
-        int r, g, b;
+        int r, g, b, SPD;
     }; struct C c[MAX];
 
     for (int i = 0; i < MAX; i++) {
         c[i].x = grid.x[genRand(0, RWIDTH / 8, gen)];
         c[i].y = grid.y[genRand(0, RHEIGHT / 8, gen)] - RHEIGHT;
-        c[i].speed = genRand(8, 32, gen);
-        c[i].delay = 1;
+        c[i].SPD = genRand(1, 4, gen);
         c[i].type[0] = { genRand(32, 127, gen) };
         c[i].type[1] = '\0';
 
@@ -78,22 +76,16 @@ int main(int argc, char* argv[]) {
                     DrawTextEx(font, c[i].type, (Vector2) { c[i].x, c[i].y - 8 }, 8, 0, Color { c[i].r, c[i].g, c[i].b, 255 });
                     DrawTextEx(font, c[i].type, (Vector2) { c[i].x, c[i].y }, 8, 0, WHITE);
 
-                    if (c[i].delay <= 0) {
+                    if (c[i].speed > 0) {
+                        c[i].speed -= GetFrameTime() * 60;
+                    } else {
                         c[i].y += 8;
                         c[i].type[0] = { genRand(32, 127, gen) };
-                        c[i].delay = 1;
-                    } else {
-                        c[i].delay -= GetFrameTime() * c[i].speed;
-                    }
+                        c[i].speed = c[i].SPD;
 
-                    if (c[i].y - 8 > RHEIGHT) {
-                        c[i].x = grid.x[genRand(0, RWIDTH / 8, gen)];
-                        c[i].y = grid.y[genRand(0, RHEIGHT / 8, gen)] - RHEIGHT;
-
-                        if (argc > 4 && std::string(argv[4]) == "true") {
-                            c[i].r = genRand(50, 255, gen);
-                            c[i].g = genRand(50, 255, gen);
-                            c[i].b = genRand(50, 255, gen);
+                        if (c[i].y - 8 > RHEIGHT) {
+                            c[i].x = grid.x[genRand(0, RWIDTH / 8, gen)];
+                            c[i].y = grid.y[genRand(0, RHEIGHT / 8, gen)] - RHEIGHT;
                         }
                     }
                 }
@@ -102,17 +94,11 @@ int main(int argc, char* argv[]) {
             }
         EndTextureMode();
         BeginDrawing();
-            DrawTexturePro(
-                surface.texture, (Rectangle) { 0, 0, float(surface.texture.width), float(-surface.texture.height) },
-                (Rectangle) { (GetScreenWidth() - float(RWIDTH) * surfaceScale) / 2,
-                (GetScreenHeight() - (float(RHEIGHT) * surfaceScale)) / 2,
-                float(RWIDTH) * surfaceScale,
-                float(RHEIGHT) * surfaceScale },
-                (Vector2) { 0, 0 }, 0, WHITE
-            );
+            DrawTexturePro(surface.texture, (Rectangle) { 0, 0, float(surface.texture.width), float(-surface.texture.height) }, (Rectangle) { (GetScreenWidth() - float(RWIDTH) * surfaceScale) / 2, (GetScreenHeight() - (float(RHEIGHT) * surfaceScale)) / 2, float(RWIDTH) * surfaceScale, float(RHEIGHT) * surfaceScale }, (Vector2) { 0, 0 }, 0, WHITE);
         EndDrawing();
 	}
 	UnloadRenderTexture(surface);
+    UnloadFont(font);
 	CloseWindow();
 	return 0;
 }
